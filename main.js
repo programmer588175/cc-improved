25033b31eb67eb43947616fe160a7a4b16804c5c
programmer588175 <48137860+programmer588175@users.noreply.github.com>
2019-04-02 15:17:19 -0400

    Always use 2 decimal places

--- main.js
+++ main.js
@@ -162,8 +162,8 @@ function Beautify(value,floats)
 	{
 	var negative=(value<0);
 	var decimal='';
-	var fixed=value.toFixed(floats);
-	if (Math.abs(value)<1000 && floats>0 && Math.floor(fixed)!=fixed) decimal='.'+(fixed.toString()).split('.')[1];
+	var fixed=value.toFixed(2);
+	if (Math.abs(value)<Infinity && floats>0 && Math.floor(fixed)!=fixed) decimal='.'+(fixed.toString()).split('.')[1];
 	value=Math.floor(Math.abs(value));
 	if (floats>0 && fixed==value+1) value++;
 	var formatter=numberFormatters[Game.prefs.format?2:1];
